efa71ff8648602a6df5521e887a4ba687cebb5b0
binarier <binarier@me.com>
2015-02-11 17:35:29 +0800

    proxy

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,26 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+chrome.runtime.onInstalled.addListener(function(details) {
+	console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeText({
+	text : '\'Allo'
+});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+var config = {
+	mode : "pac_script",
+	pacScript : {
+		data : "function FindProxyForURL(url, host) {\n"
+				+ "  if (shExpMatch(host, '*.google.*'))\n"
+				+ "    return 'PROXY 211.149.217.191:7777';\n"
+				+ "  return 'DIRECT';\n"
+				+ "}"
+	}
+};
+chrome.proxy.settings.set({
+	value : config,
+	scope : 'regular'
+});
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,21 @@
 'use strict';
 
+$("#btn-always").click(function(){
+var config = {
+		mode : "pac_script",
+		pacScript : {
+			data : "function FindProxyForURL(url, host) {\n"
+					+ "  if (shExpMatch(host, '*.google.*'))\n"
+					+ "    return 'PROXY 211.149.217.191:7777';\n"
+					+ "  return 'DIRECT';\n"
+					+ "}"
+		}
+	};
+	chrome.proxy.settings.set({
+		value : config,
+		scope : 'regular'
+	});
+});
+
 console.log('\'Allo \'Allo! Popup');
+
